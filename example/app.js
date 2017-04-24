28d1d83c93bd7441cb6c86e697c254356e279907
David Picarra <daviddcp@gmail.com>
2017-04-24 10:57:10 +0100

    change example app.js to import prod version

--- example/app.js
+++ example/app.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import VueAlert from '../dist/vuealert.common.js'
 import Example from './Example.vue'
 
-const VueAlert = process.env.NODE_ENV === 'production' ? require('../dist/vuealert.common.js').default : require('../src/index.js').default
-
-Vue.use(VueAlert)
+if (process.env.NODE_ENV !== 'production') {
+  const VueAlertDev = require('../src/index.js').default
+  Vue.use(VueAlertDev)
+} else {
+  Vue.use(VueAlert)
+}
 
 /* eslint-disable no-new */
 new Vue({
