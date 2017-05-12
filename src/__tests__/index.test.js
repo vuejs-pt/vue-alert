cf235f29879defaa7656713e4d5763b3ef27c5a1
David Picarra <daviddcp@gmail.com>
2017-05-12 15:05:50 +0100

    yarn upgrade

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -7,7 +7,7 @@ describe('vuealert.min.js', () => {
     const vm = new Vue({
       template: '<div id="app"><vue-alert></vue-alert></div>'
     }).$mount()
-    expect(vm.$alert).not.toEqual(null)
+    expect(vm.$alert).not.toBeNull()
   })
   it('should warn if there is no vue-alert component', () => {
     global.console = { warn: jest.fn() }
@@ -15,7 +15,7 @@ describe('vuealert.min.js', () => {
     const vm = new Vue({
       template: '<div></div>'
     }).$mount()
-    expect(vm.$alert).toEqual(null)
+    expect(vm.$alert).toBeNull()
     expect(console.warn).toBeCalledWith('Vue-alert component must be part of this component scope or any of the parents scope.')
   })
 })
